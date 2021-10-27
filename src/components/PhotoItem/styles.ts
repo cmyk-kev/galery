d4d1c37ea06159094ba432cc8ff33d23648b0fcb
Kevin da Silva Felix <kevinsfelix@outlook.com>
2021-10-26 21:42:03 -0300

    arquivos do projeto

---
+++ src/components/PhotoItem/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background: #3D3F43;
+  border-radius: 10px;
+  padding: 10px;
+  
+ 
+
+  img{ 
+    width: 50%;
+    display: block;
+    margin-bottom: 10px;
+    border-radius: 10px;
+  }
+
+`;
